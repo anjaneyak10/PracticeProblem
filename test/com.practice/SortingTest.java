12fef2ff0741857119d2f6039364e0ae71ae5060
Ameya <ketkara@oegonstate.edu>
2020-03-16 14:15:23 -0700

    tests

--- test/com.practice/SortingTest.java
+++ test/com.practice/SortingTest.java
@@ -1,4 +1,88 @@
 package com.practice;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.practice.Sorting.*;
+
+
 public class SortingTest {
+
+    @Test
+    public void testIsSorted(){
+
+        // Failing test
+        int[] emptyArray = {};
+        Assertions.assertTrue(isSorted(emptyArray));
+
+        int[] array2 = {1,2};
+        Assertions.assertTrue(isSorted(array2));
+
+        // Failing test
+        int[] array2Rev = {2,1};
+        Assertions.assertTrue(isSorted(array2Rev));
+
+        int[] array3Sorted = {1,2,3};
+        Assertions.assertTrue(isSorted(array3Sorted));
+
+        // Failing test
+        int[] array3NotSorted = {1,3,2};
+        Assertions.assertFalse(isSorted(array3NotSorted));
+
+    }
+
+
+    @Test
+    public void testSort(){
+        int[] emptyArray = {};
+        Assertions.assertTrue(isSorted(sort(emptyArray)));
+
+        int[] array1 = {1};
+        Assertions.assertTrue(isSorted(sort(array1)));
+
+        int[] array2 = {1,2};
+        Assertions.assertTrue(isSorted(sort(array2)));
+
+        // Assuming the sort is akways ascending
+        int[] array2_ = {2,1};
+        Assertions.assertTrue(isSorted(sort(array2)));
+
+        // Some fun testing
+
+        int randomTests = 5;
+
+        int[] randomArrayLengths = TestUtil.createRandomArray(randomTests);
+
+        for (int len : randomArrayLengths){
+            Assertions.assertTrue(isSorted(sort(TestUtil.createRandomArray(len))));
+        }
+    }
+
+//    @Test
+//    public void testMergeSort(){
+//        int[] emptyArray = {};
+//        Assertions.assertTrue(isSorted(mergeSort(emptyArray)));
+//
+//        int[] array1 = {1};
+//        Assertions.assertTrue(isSorted(mergeSort(array1)));
+//
+//        int[] array2 = {1,2};
+//        Assertions.assertTrue(isSorted(mergeSort(array2)));
+//
+//        // Assuming the sort is akways ascending
+//        int[] array2_ = {2,1};
+//        Assertions.assertTrue(isSorted(mergeSort(array2)));
+//
+//        // Some fun testing
+//
+//        int randomTests = 5;
+//
+//        int[] randomArrayLengths = TestUtil.createRandomArray(randomTests);
+//
+//        for (int len : randomArrayLengths){
+//            Assertions.assertTrue(isSorted(mergeSort(TestUtil.createRandomArray(len))));
+//        }
+//    }
+
+
 }
--- test/com.practice/TestUtil.java
+++ test/com.practice/TestUtil.java
@@ -1,4 +1,22 @@
 package com.practice;
 
+import java.util.Random;
+
 public class TestUtil {
+
+
+    public static int[] createRandomArray(int length) {
+        Random rd = new Random();
+        int[] arr = new int[length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rd.nextInt();
+        }
+        return arr;
+    }
+
+    public static int getRandomInt(int max) {
+        Random rd = new Random();
+        return rd.nextInt(max);
+    }
+
 }
